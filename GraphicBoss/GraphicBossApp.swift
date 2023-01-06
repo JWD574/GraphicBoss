090ee525dc7261b9e189c41baaaa70e081c5f841
rangerboy574 <jdeweese574@gmail.com>
2023-01-05 20:12:06 -0600

    Initial Commit

---
+++ GraphicBoss/GraphicBossApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicBossApp.swift
+//  GraphicBoss
+//
+//  Created by Joseph  DeWeese on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphicBossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
